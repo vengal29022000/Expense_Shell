77c141d45f26d3489f06f78fedeea01aa0dae8db
VENGALAREDDY <vengalareddy.challa@gmail.com>
2025-01-08 02:05:25 +0530

    Expense Project

--- Application.sh
+++ Application.sh
@@ -48,7 +48,7 @@ fi
 mkdir -p /app &>>$LOG_FILE_NAME
 VALIDATE "/app"
 
-curl -o curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE "Downloading Backend..."
 
 cd /app &>>$LOG_FILE_NAME               
